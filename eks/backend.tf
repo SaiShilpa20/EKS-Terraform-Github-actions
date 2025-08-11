9053f2ca18cfa9699a258f15c3cb57ae04b8f867
SaiShilpa20 <silpathoga16@gmail.com>
2025-08-11 11:06:50 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.9.3"
+  required_version = "~> 1.12.2"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-jenkins-bucket-16"
+    bucket         = "jenkins-server-16-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
@@ -17,4 +17,5 @@ terraform {
 
 provider "aws" {
   region  = var.aws-region
+
 }
